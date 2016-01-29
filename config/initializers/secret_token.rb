f6ae37b3a093ef81e1c34bfa19bffae48e3a1ff0
Drew Kario <kario@apostrophed.com>
2016-01-29 11:40:25 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ggj2016::Application.config.secret_token = '8feeed13d15e997ae720802fa82eaedaea8b2f8e7d95519c25ec33b8a92d0e3a74695b74a1115dfcae9c5f1db46a82a1070ad80abd1bf745df9a9f4a94150edd'
