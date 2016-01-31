caf592cdac9c7a2144cf1ddadbb2ad74fcc2ebea
Drew Kario <kario@apostrophed.com>
2016-01-31 10:56:27 -0500

    Adjusted production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Ggj2016::Application.configure do
   config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  #config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = false
